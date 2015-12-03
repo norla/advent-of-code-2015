4f8d2a27dc215107232aac115b9494690d0c82dc
Mattias Norlander <mattias.norlander@gmail.com>
2015-12-03 15:01:49 +0100

    WIP

--- solution-3.ex
+++ solution-3.ex
@@ -1,4 +1,4 @@
-defmodule Solution3 do
+defmodule Solution3_1 do
 
   def run do
     acc = {{0, 0}, HashDict.new}
@@ -17,4 +17,21 @@ defmodule Solution3 do
     {newPos, Dict.put(map, newPos, 1)}
   end
 
+  def run2 do
+    acc = {[{0, 0}, {0, 0}], HashDict.new}
+    {_, map} = File.stream!("input-3.txt", [], 1) |> Enum.reduce(acc, &move2/2)
+    Dict.size(map)
+  end
+
+  def move2(dir, {{xPos, yPos}, other}) do
+    newPos = case dir do
+      "^" -> {xPos    , yPos + 1}
+      "v" -> {xPos    , yPos - 1}
+      ">" -> {xPos + 1, yPos    }
+      "<" -> {xPos - 1, yPos    }
+      _   -> {xPos    , yPos    }
+    end
+  {{other, newPos}, Dict.put(map, newPos, 1)}
+  
 end
+
