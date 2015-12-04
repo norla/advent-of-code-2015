94a6ff8630801aca9f245002ebfc4879c6c292a3
Mattias Norlander <mattias.norlander@gmail.com>
2015-12-04 12:31:02 +0100

    More readable

--- src/solution-1.ex
+++ src/solution-1.ex
@@ -1,12 +1,15 @@
 defmodule Solution1 do
+
   def run do
-    moves = IO.read(:stdio, :all) |> String.strip |> String.codepoints |>
-            Enum.map(fn("(") -> 1; (")") -> -1 end)
-    finalFloor = Enum.sum(moves)
-    movesTilBasement = Enum.reduce_while(moves, {0, 1}, &basement/2)
+    deltas =
+      IO.read(:stdio, :all) |> String.strip |> String.codepoints
+      |> Enum.map(fn("(") -> 1; (")") -> -1 end)
+    finalFloor = Enum.sum(deltas)
+    movesTilBasement = Enum.reduce_while(moves, {0, 0}, &basement/2)
     IO.inspect([part1: finalFloor, part2: movesTilBasement])
   end
 
-  defp basement(-1, {0, n}), do: {:halt, n}
+  defp basement(_, {-1, n}), do: {:halt, n}
   defp basement(delta, {floor, n}), do: {:cont, {floor + delta, n + 1}}
+
 end
--- src/solution-2.ex
+++ src/solution-2.ex
@@ -1,21 +1,22 @@
 defmodule Solution2 do
 
   def run do
-    input = IO.read(:stdio, :all)
-    IO.inspect([part1: run(&paper/1, input), part2: run(&ribbon/1, input)])
+    boxes =
+      IO.read(:stdio, :all) |> String.strip |> String.split |> Enum.map(&box/1)
+    IO.inspect([part1: run(&paper/1, boxes), part2: run(&ribbon/1, boxes)])
   end
 
-  defp run(calc, inout) do
-    String.split(input, "\n") |> Enum.map(&sides/1) |> Enum.map(calc) |> Enum.sum
+  defp run(calc, box) do
+    box |> Enum.map(calc) |> Enum.sum
   end
 
-  defp sides(""), do: [0, 0, 0]
-  defp sides(line) do
-    String.strip(line) |> String.split("x") |> Enum.map(&String.to_integer/1) |> Enum.sort
+  defp box(line) do
+    line |> String.strip |> String.split("x") |> Enum.map(&String.to_integer/1)
+    |> Enum.sort
   end
 
   defp paper([s, m, l]), do: (2 * s * m) + (2 * m * l) + (2 * s * l) + (s * m)
 
   defp ribbon([s, m, l]), do: (2 * s) + (2 * m) + (s * m * l)
-
+  
 end
