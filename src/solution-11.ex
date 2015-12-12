72942d12e45aca80d7e624748b6e3808fabc6d68
Mattias Norlander <norla@campanja.com>
2015-12-12 09:25:14 +0100

    Working 11

--- src/solution-11.ex
+++ src/solution-11.ex
@@ -2,36 +2,34 @@ defmodule Solution11 do
 
   def run do
     pwd = IO.read(:stdio, :all) |> String.strip
-    IO.inspect(try_pwd(inc(pwd, 7)))
+    pwd1 = next_pwd(pwd)
+    pwd2 = next_pwd(pwd1)
+    IO.inspect([part1: pwd1, part2: pwd2])
   end
 
-  def try_pwd(pwd) do
-    case check(pwd, %{pairs: 0, straight: 0, pos: 0, ok: false}) do
-      %{ok: false, pos: pos} ->
-        IO.puts(pwd)
-        try_pwd(inc(pwd, pos - 1))
-      
-      state -> {state,pwd}
-    end
+  def next_pwd(pwd) do
+    next = inc(pwd)
+    if check(next, %{pairs, 0, straight: 0}), do: next, else: next_pwd(next)
   end
 
-  def check(<<>>, state), do: %{state | :ok => state.pairs > 1 && state.straight > 0}
-  def check(<<x, _::binary>>, state) when (x == ?l or x == ?o or x == ?l), do: state
+  def check(<<>>, state), do: state.pairs > 1 && state.straight > 0
+  def check(<<x, _::binary>>, _) when (x == ?l or x == ?o or x == ?l), do: false
   def check(<<x, x, x, tail::binary>>, state)  when x == x do
-    check(<<x, tail::binary>>, %{state | pairs: state.pairs + 1, pos: state.pos + 2})
+    check(<<x, tail::binary>>, %{state | pairs: state.pairs + 1})
   end
   def check(<<x, y, tail::binary>>, state) when x == y do
-    check(<<x, tail::binary>>, %{state | pairs: state.pairs + 1, pos: state.pos + 1})
+    check(<<x, tail::binary>>, %{state | pairs: state.pairs + 1})
   end
   def check(<<x, y , z, tail::binary>>, state) when (y == x + 1 and z == y + 1) do
-    check(<<y, z, tail::binary>>, %{state | straight: 1, pos: state.pos + 1})
+    check(<<y, z, tail::binary>>, %{state | straight: 1})
   end
-  def check(<<_, tail::binary>>, state), do: check(tail, %{state | pos: state.pos + 1})
+  def check(<<_, tail::binary>>, state), do: check(tail, state)
 
-  def inc(pwd, pos) do
-    pwd |> String.codepoints |> Enum.slice(0..pos)
-    |> List.update_at(pos, fn (<<c>>) -> <<rem((c - 96), 26) + 97>> end)
-    |> Enum.concat(List.duplicate("a", 7 - pos)) |> Enum.join
-  end
+  def inc(pwd) do
+    Regex.replace(~r/(.*)([a-y])(z*)$/s, pwd,
+      fn (_,x,<<y>>,z) ->
+        <<x::binary, rem((y - 96), 26) + 97, (String.replace(z, "z", "a"))::binary>>
+      end)
+   end
 
 end
