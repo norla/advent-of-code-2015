a38126490750c7f85d11fddeb4a31f095c2e4505
Mattias Norlander <mattias.norlander@gmail.com>
2015-12-14 14:56:46 +0100

    Day 14

---
+++ lib/solution-14.ex
@@ -0,0 +1,34 @@
+defmodule Solution14 do
+
+  def run do
+    input = IO.read(:stdio, :all) |> String.strip |> String.split("\n")
+    reindeers = input |> Enum.reduce(HashDict.new(), &parse/2)
+    res1 = Enum.map(reindeers, &distance(&1, 2503)) |> top
+    res2 = 1..2503 |> Enum.reduce(reindeers, &score/2) |> Enum.map(fn({_, x}) -> x.score end) |> top
+    IO.inspect([part1: res1, part2: res2])
+  end
+
+  def parse(line, acc) do
+    [_, g1, g2, g3 ,g4] = Regex.run(~r/(\w+).* (\d+) .* (\d+) .* (\d+) /, line)
+    Dict.put(acc, g1, %{speed: to_i(g2), fly: to_i(g3), rest: to_i(g4), score: 0})
+  end
+
+  def distance({_, %{speed: speed, fly: fly, rest: rest}}, secs) do
+    div(secs, fly + rest) * speed * fly + min(fly, rem(secs, fly + rest)) * speed
+  end
+
+  def score(secs, reindeers) do
+    list = reindeers |> Enum.map(&distance(&1, secs)) |> Enum.zip(reindeers) |> rsort
+    {maxDist, _} = List.first(list)
+    leaders = Enum.take_while(list, fn({dist, _}) -> dist == maxDist end)
+    leaders |> Enum.reduce(reindeers,
+      fn({_, {key, reindeer}}, acc) ->
+        Dict.put(acc, key, %{reindeer | score: reindeer.score + 1})
+      end)
+  end
+
+  def to_i(x), do: String.to_integer(x)
+  def top(list), do: list |> Enum.sort |> List.last
+  def rsort(list), do: list |> Enum.sort |> Enum.reverse
+
+end
